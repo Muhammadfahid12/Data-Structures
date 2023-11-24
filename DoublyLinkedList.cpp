358fc2eb06c8a6322653718f39bfe0daf878e7ce
Muhammad Fahid <ranafahadirshad@gmail.com>
2023-11-24 20:25:48 +0500

    getNode function

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -25,24 +25,60 @@ class DoublyList
         }
     };
 
-    Node* head;
-    Node* tail;
+    Node *head;
+    Node *tail;
     int listSize = 0;
 
-   public:
-            DoublyList()
-            {
-             head = NULL;
-             tail = NULL;
-             listSize = 0;
-            } 
+    Node *getNode(int index)
+    {
+        Node *n = NULL;
 
-            DoublyList(void* data)
+        if (index >= 0)
+        {
+            n = this->head;
+            for (int i = 0; i < index; i++)
             {
-                
+                n = n->next;
             }
+            return n;
+        }
+        else
+        {
+            n = this->tail;
+            for (int i = -1; i > index; i--)
+            {
+                n = n->prev;
+            }
+        }
+    }
 
+public:
+    DoublyList()
+    {
+        head = NULL;
+        tail = NULL;
+        listSize = 0;
+    }
 
+    DoublyList(void *data)
+    {
+        this->head = new Node(data);
+        this->tail = this->head;
+        this->listSize = 1;
+    }
 
+    // getting an element through index;
 
+    void *read(int index = -1)
+    {
+        if (index >= 0)
+        {
+            Node *n = this->head;
+
+            for (int i = 0; i < index; i++)
+            {
+                n = n->next;
+            }
+        }
+    }
 };
